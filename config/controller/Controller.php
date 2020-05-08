b9f87304558794091a9565bdfafc2dbc2677d2f7
Gisacc54 <giftisacc54@gmail.com>
2020-05-08 13:03:56 +0300

    more option

--- config/controller/Controller.php
+++ config/controller/Controller.php
@@ -1,7 +1,10 @@
 <?php
 namespace Config\Controller;
-
+/*
+$this->more(['users'=>$users])->view('welcome'); view with parameters in controller files
+*/
 class Controller {
+  public $variable;
   public function view($view)
   {
     if(file_exists("resource/views/$view.php") ){
@@ -13,10 +16,18 @@ class Controller {
       $message ="File resource/views/$view.php is not Found. Pleas create view file in 'resource/views/' directory ";
 
       // include "Exceptions/error.php";
+
       include "Exceptions/error.php";
       return;
     }
 
+  }
+
+  public function more($valiables)
+  {
+    $obj=$this;
+    $obj->variable=$valiables;
+    return $obj;
   }
 
 }
